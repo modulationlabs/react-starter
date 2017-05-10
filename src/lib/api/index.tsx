67e0a4ec433a20a9449f4c9a578496bae10e210c
Christopher Hill <accounts@christopherhill.com>
2017-05-10 10:14:18 -0700

    WIP API interface.

---
+++ src/lib/api/index.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import * as _ from 'lodash';
+
+export class Http {
+
+  public headers: Headers;
+  public defaultOptions: RequestInit;
+
+  constructor() {
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json');
+    this.defaultOptions = {
+      headers: this.headers,
+      mode: 'cors',
+      cache: 'default'
+    }
+  }
+
+  options(additionalObject: any) {
+    return Object.assign({}, this.defaultOptions, additionalObject);
+  }
+
+  get(url: string, options: RequestInit) {
+    return fetch(url, options);
+  }
+
+  post(url: string, body: Object, options: any) {
+    return fetch(url, options({ method: 'POST' }));
+  }
+
+  patch(url: string, body: Object, options: any) {
+    return fetch(url, options({ method: 'PATCH' }));
+  }
+
+  put(url: string, body: Object, options: any) {
+    return fetch(url, options({ method: 'PUT' }));
+  }
+
+  del(url: string, options: any) {
+    return fetch(url, options({ method: 'DELETE' }));
+  }
+
+}
